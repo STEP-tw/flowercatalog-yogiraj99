4962f4087059bd5077c37d1208226a49aec954bf
yogiraj_tambake <ytambake@gmail.com>
2018-01-09 15:09:15 +0530

    working flowerCatlogServer using webapp except for post comment

---
+++ server.js
@@ -0,0 +1,34 @@
+const webapp = require('./webapp.js');
+const http = require('http');
+const fs = require('fs');
+const Comments = require('./lib/comments.js');
+const guestBookSync=require('./lib/guestBook.js').guestBookSync;
+const doesExistInPublic = require('./lib/responseToFile.js').doesExistInPublic;
+const giveContentInResponse = require('./lib/responseToFile.js').giveContentInResponse;
+
+const PORT=8000;
+
+let comments=new Comments("./data/comments.JSON");
+
+let app=webapp.create();
+
+let redirectSlashToIndexPage = function (req,res) {
+  if (req.url=='/') res.redirect('/index.html');
+}
+
+let fileServer = function (req,res) {
+  if (doesExistInPublic(req.url)) {
+    giveContentInResponse(req.url,res);
+  }
+}
+
+app.use(redirectSlashToIndexPage);
+app.use(fileServer);
+app.get("/guestBook",(req,res)=>{
+  comments.loadComments();
+  guestBookSync(res,comments);
+})
+
+let server=http.createServer(app);
+server.listen(PORT);
+console.log(`listening at ${PORT}`);
